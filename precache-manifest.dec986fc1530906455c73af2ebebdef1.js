f5e06e46c8dac64acf3d493fd67a9f0426b20332
lachmed <53819069+lachmed@users.noreply.github.com>
2020-11-09 06:30:48 +0100

    Updates

---
+++ precache-manifest.dec986fc1530906455c73af2ebebdef1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "098751a73e7a4f2768e18269fa3d7cba",
+    "url": "/Transport/index.html"
+  },
+  {
+    "revision": "6efc705917e71e744ef4",
+    "url": "/Transport/static/css/main.0c50c0d1.chunk.css"
+  },
+  {
+    "revision": "cebbecc7230bdc7eae67",
+    "url": "/Transport/static/js/2.fe2a0654.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/Transport/static/js/2.fe2a0654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6efc705917e71e744ef4",
+    "url": "/Transport/static/js/main.7714dfa5.chunk.js"
+  },
+  {
+    "revision": "e9b27a2978378daaeb6b",
+    "url": "/Transport/static/js/runtime-main.44e89343.js"
+  }
+]);
